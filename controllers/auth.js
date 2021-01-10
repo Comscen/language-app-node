1b7d3bff7c0e352dd82533cb10097f11406dab41
bastianek321 <czaj.sebastian@gmail.com>
2021-01-10 21:05:16 +0100

    Add login, register and logout functionalities

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,31 @@
+const firebaseService = require('../database')
+
 exports.showLoginForm = (req, res) => {
-    return res.render('login.ejs');
+    if(typeof req.session.idToken != 'undefined'){
+        return res.render('index.ejs', {session:req.session, error: 'Jesteś już zalogowany!'})
+    }
+    return res.render('login.ejs', {session: req.session});
 }
 
-exports.handleEmailLogin = (req, res) => {
-    let user;
-    await firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
-        .then(object => {
-            user = object;
+exports.handleEmailLogin = async (req, res) => {
+    firebaseService.signInWithEmailAndPassword(req.body.email, req.body.password).then(user => {
+        req.session.uid = user.user.uid;
+        user.user.getIdToken(true).then(token => {
+            req.session.idToken = token
+            return res.render('index.ejs', {message: 'Pomyślnie zalogowano!', session: req.session })
+        }).catch(error => {
+            console.log(`BLAD TOKENU ${error}`)
         })
-        .catch(error => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            if (errorCode === 'auth/wrong-password') {
-                console.log('Wrong password.');
-            } else {
-                console.log(errorMessage);
-            }
-            console.log(error);
-        })
-    return user //todo
+    }).catch(error => {
+        switch (error.code) {
+            case 'auth/wrong-email':
+                return res.render('register.ejs', { error: 'Niepoprawny e-mail lub hasło.', session: req.session })
+            case 'auth/wrong-password':
+                return res.render('register.ejs', { error: 'Niepoprawny e-mail lub hasło.', session: req.session })
+            default:
+                console.log(`UNKNOWN ERROR: ${error}`);
+        }
+    })
 }
 
 exports.handleGoogleLogin = (req, res) => {
@@ -31,28 +37,46 @@ exports.handleFacebookLogin = (req, res) => {
 }
 
 exports.showRegisterForm = (req, res) => {
-    return res.render('register.ejs');
+    if(typeof req.session.idToken != 'undefined'){
+        return res.render('index.ejs', {session:req.session, error: 'Jesteś już zalogowany! Nie możesz utworzyć konta.'})
+    }
+    return res.render('register.ejs', {session: req.session});
 }
 
-exports.handleRegisterForm = (req, res) => {
-    let user;
-    await firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password).then(user => {
-        firebase.firestore().doc(`users/${user.user.uid}`).set({ wordAmount: 0 }).
-            then(result => {
-                user = result;
-            })
-            .catch(error => {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                if (errorCode == 'auth/weak-password') {
-                    console.log('The password is too weak.');
-                } else {
-                    console.log(errorMessage);
-                }
-                console.log(error);
-            });
+exports.handleRegisterForm = async (req, res) => {
+    
 
-    })
-    return user //todo
+    if (req.body.password !== req.body.repeatPassword) {
+        return res.render('register.ejs', { error: 'Podane hasła się nie zgadzają!', session: req.session });
+    }
+    firebaseService.signUpWithEmailAndPassword(req.body.email, req.body.password).then(user => {
+        firebaseService.firebase.firestore().doc(`users/${user.user.uid}`).set({ wordAmount: 0 }).then(_ => {
+            req.session.uid = user.user.uid;
+            user.user.getIdToken(true).then(token => {
+                req.session.idToken = token;
+                return res.render('index.ejs', {message: 'Pomyślnie zarejestrowano! Zostałeś automatycznie zalogowany.', session: req.session })
+            }).catch(error => {
+                console.log(`BLAD TOKENU ${error}`)
+            })
+        }).catch(error => {
+            console.log(`BLAD FIRESTORE ${error}`)
+        })
+    }).catch(error => {
+        switch (error.code) {
+            case 'auth/email-already-in-use':
+                return res.render('register.ejs', { error: 'Konto o danym adresie e-mail już istnieje!', session: req.session })
+            case 'auth/weak-password':
+                console.log(error.message)
+                return res.render('register.ejs', { error: 'Hasło musi zawierać co najmniej 6 znaków!', session: req.session })
+            default:
+                console.log(`UNKNOWN ERROR: ${error}`);
+        }
+    });
+}
+
+exports.logout = async (req, res) => {
+    await firebaseService.deleteSession(req.session.uid);
+    req.session.uid = undefined;
+    req.session.idToken = undefined;
+    return res.render('index.ejs', { message: 'Pomyślnie wylogowano.', session: req.session });
 }
